4690ac07c5beccee2ef2ae24a6cd27c09b7aa44b
aashutoshnishad147@gmail.com <ashutoshnishad2003@gmail.com>
2023-07-16 17:54:42 +0530

    add notice board

--- app.js
+++ app.js
@@ -11,3 +11,39 @@ hlink.forEach(link=>{
         hamburger.classList.toggle("bg-white")
     })
 })
+
+var data = "https://script.google.com/macros/s/AKfycbxV9bz-cFZSC7LGIuWRWlmaHJhKpT5aDNiKlD4G5SUPdyoHC2oh2pNnHWlIBVZABIAF/exec";
+
+fetch(data)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  })
+  .then(data => {
+    // Process the JSON data
+    // console.log(data);
+    writedata(data)
+  })
+  .catch(error => {
+    // Handle any errors that occurred during the fetch
+    console.error('Error:', error);
+  });
+
+
+
+
+var writedata = (data)=>{
+
+var noticeboard = document.getElementsByClassName("link-list")[0];
+noticeboard.style.animationDuration =  (data.length + data.length/2 + 1)+"s";
+for (let index = data.length - 1 ; index >= 0 ; index--) {
+    const element = data[index];
+    // element.date = "12-05-2003"
+    let comp = document.createElement("li");
+    comp.style.padding = '10px'
+    comp.innerHTML =`<a href=${element.link}>${element.title + " || "+"( " + element.date + " )"}</a>`
+    noticeboard.appendChild(comp);
+}
+}
